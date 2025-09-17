fc92471dc2dbc13c56780bc58a1e4110d523eeaf
Oswaldo <U22209742@utp.edu.pe>
2025-09-16 20:22:57 -0500

    feat:Instalacion de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('ApuchaWatchAPI')
+    .setVersion('1.0')
+    .setDescription('API for ApuchaWatch application')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    documentationConfig,
+  );
+  SwaggerModule.setup('api', app, swaggerDocument);
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();

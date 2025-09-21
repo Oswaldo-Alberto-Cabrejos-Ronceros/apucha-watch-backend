c95f2bd8f9d4321dd6259ef1bf7ac2d1357f620d
Oswaldo <U22209742@utp.edu.pe>
2025-09-21 14:46:19 -0500

    feat:desarrollo del guard de autenticacion basada en supabase

---
+++ src/auth/guards/supabase-auth-guard/supabase-auth-guard.guard.ts
@@ -0,0 +1,41 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { AuthenticationRequest } from 'src/common/models/authentication-request.model';
+
+@Injectable()
+export class SupabaseAuthGuardGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly supabaseClient: SupabaseClient,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    //if is public allow access
+    if (isPublic) {
+      return true;
+    }
+    //for verification of token with supabase auth
+    const req: AuthenticationRequest = context.switchToHttp().getRequest();
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      throw new UnauthorizedException('No token provided');
+    }
+    const token = authHeader.replace('Bearer ', '');
+
+    const { data, error } = await this.supabaseClient.auth.getUser(token);
+    if (error || !data?.user) {
+      throw new UnauthorizedException('No token provided');
+    }
+    req.user = data.user;
+    return true;
+  }
+}
---
+++ src/common/models/authentication-request.model.ts
@@ -0,0 +1,4 @@
+import { Request } from 'express';
+export interface AuthenticationRequest extends Request {
+  user?: any;
+}

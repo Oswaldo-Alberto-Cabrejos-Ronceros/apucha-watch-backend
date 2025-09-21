0375798a408de455867bec3fbd984006f488f489
Oswaldo <U22209742@utp.edu.pe>
2025-09-21 16:46:16 -0500

    fix:Arreglo en importar el typeorm de CarerPrifle en AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { SupabaseModule } from 'src/supabase/supabase.module';
 import { CarerProfleModule } from 'src/carer-profle/carer-profle.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CarerProfle } from 'src/carer-profle/entities/carer-profle.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [SupabaseModule, CarerProfleModule],
+  imports: [
+    TypeOrmModule.forFeature([CarerProfle]),
+    SupabaseModule,
+    CarerProfleModule,
+  ],
 })
 export class AuthModule {}

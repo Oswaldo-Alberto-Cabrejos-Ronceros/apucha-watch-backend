bcf65267091e8d1078d6dd4fee543d51592d7f8f
Adrián Vega <adrian.vegaf14@gmail.com>
2025-09-29 16:24:27 -0500

    fix: Correccion de la entidad de vital-signs

--- src/vital-signs/entities/vital-sign.entity.ts
+++ src/vital-signs/entities/vital-sign.entity.ts
@@ -1,25 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import { Device } from '../../device/entities/device.entity';
 
-@Entity()
+@Entity({ name: 'vital_signs' })
 export class VitalSign {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    deviceCode: string;
+  @Column()
+  deviceCode: string;
 
-    @Column({ type: 'timestamp' })
-    timestamp: Date;
+  @Column({ type: 'timestamp' })
+  timestamp: Date;
 
-    @Column('int')
-    heartRate: number;
+  @Column('int')
+  heartRate: number;
 
-    @Column('int')
-    oxigenSaturation: number;
+  @Column('int')
+  oxigenSaturation: number;
 
-    @ManyToOne(() => Device, (device) => device.vitalSigns)
-    @JoinColumn({ name: 'deviceCode', referencedColumnName: 'code' })
-    device: Device;
+  @ManyToOne(() => Device, (device) => device.vitalSigns)
+  @JoinColumn({ name: 'deviceCode', referencedColumnName: 'code' })
+  device: Device;
 }
-

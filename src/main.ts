fd2fce6f84c03dd01f94b030af508ebd950e6f3a
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-10-07 14:25:56 -0500

    feat:Primera version de supabase auth middleware

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { TypeORMNotFoundInterceptor } from './common/interceptors/typeorm-not-found.interceptor';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { SupabaseAuthService } from './supabase/supabase-auth.service';
+import { SocketAuthMiddleware } from './common/middlewares/socket-auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //for register socketMiddleware global
+  //socket io con cors
+  const adapter = new IoAdapter(app);
+  app.useWebSocketAdapter(adapter);
+  //get supabaseAuthService
+  const supabaseAuthService = app.get(SupabaseAuthService);
+  const socketAuthMiddleware = new SocketAuthMiddleware(supabaseAuthService);
+  //access to native server nest
+  const httpServer = app.getHttpServer();
+  const io = require('socket.io')(httpServer, {
+    cors: { origin: '*' },
+  });
+  //apply middleware
+  io.use =(socketAuthMiddleware.use);
   app.useGlobalInterceptors(new TypeORMNotFoundInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({

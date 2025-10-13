70c0607ac9f936c83fa1efa6c8bad90e602f46cb
Oswaldo <U22209742@utp.edu.pe>
2025-10-13 15:36:58 -0500

    feat:Desarrollo de subscripcion a sala en fall gateway

--- src/fall/fall.gateway.ts
+++ src/fall/fall.gateway.ts
@@ -1,11 +1,28 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { CreateFallDto } from './dto/create-fall.dto';
+import { DeviceRoomService } from 'src/device-room/device-room.service';
 @WebSocketGateway({ cors: { origin: '*' } })
 export class FallGateway {
   @WebSocketServer() server: Server;
+  constructor(private readonly deviceRoomDevice: DeviceRoomService) {}
+
+  @SubscribeMessage('subscribe-fall')
+  handleSubscribe(
+    @MessageBody() data: { deviceCode: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    return this.deviceRoomDevice.subscribeToDevice(client, data.deviceCode);
+  }
+
   //for emit event for fall
   sendFall(data: CreateFallDto) {
-    this.server.emit('fall-detected', data);
+    this.server.to(data.deviceCode).emit('fall-detected', data);
   }
 }

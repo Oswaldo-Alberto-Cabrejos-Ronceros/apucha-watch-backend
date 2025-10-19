d60253a48e1a1b327495ff94f025dc3f1f7ada64
Oswaldo <U22209742@utp.edu.pe>
2025-10-18 21:48:19 -0500

    feat:Adicion de validaciones para dto create-senior-citizen-health-condition

--- src/senior-citizen-health-condition/dto/create-senior-citizen-health-condition.dto.ts
+++ src/senior-citizen-health-condition/dto/create-senior-citizen-health-condition.dto.ts
@@ -1,8 +1,19 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsEnum, IsInt, IsPositive } from 'class-validator';
 import { ConditionSeverity } from '../enums/contition-severity.enum';
 
 export class CreateSeniorCitizenHealthConditionDto {
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
   seniorCitizenProfileId: number;
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
   healthConditionId: number;
+  @Type(() => Date)
+  @IsDate()
   diagnosisDate: Date;
+  @IsEnum(ConditionSeverity)
   severity: ConditionSeverity;
 }
